5c409bfb109a8b643d74e02aed91b284b8c4c18d
Bui Minh Toan <toan.bui@nexpando.com>
2024-04-26 23:02:10 +0700

    day01

--- jdk8/NumberAndStrings/strings/ComputeResult.java
+++ jdk8/NumberAndStrings/strings/ComputeResult.java
@@ -6,11 +6,11 @@ public class ComputeResult {
         StringBuilder result = new StringBuilder("hi");
         int index = original.indexOf('a');
 
-// /*1*/   result.setCharAt(0, original.charAt(0));// h --> s => now is 'si'
-/*2*/   result.setCharAt(1, original.charAt(original.length()-1));// i --> r => now is 'sr'
-// /*3*/   result.insert(1, original.charAt(4));// add w into 'sr' => now is 'srw'
-// /*4*/   result.append(original.substring(1,4));// => now is 'srwoft'
-// /*5*/   result.insert(3, (original.substring(index, index+2) + " ")); // now is 'srware oft'
+/*1*/   result.setCharAt(0, original.charAt(0));// h --> s => now is 'si'
+/*2*/   result.setCharAt(1, original.charAt(original.length()-1));// i --> e => now is 'se'
+/*3*/   result.insert(1, original.charAt(4));//se + "w" --> swe => now is 'swe'
+/*4*/   result.append(original.substring(1,4));// swe + "oft" --> now is "sweoft"
+/*5*/   result.insert(3, (original.substring(index, index+2) + " ")); // sweoft  + "ar " --> swear oft => now is "swear oft"
 
         System.out.println("result is: " + result);
     }
