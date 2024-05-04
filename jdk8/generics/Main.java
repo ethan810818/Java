cc1e558e17946f0321e21df53a70ee7475d67b34
Bui Minh Toan <toan.bui@nexpando.com>
2024-05-04 13:31:53 +0700

    pending generics

--- jdk8/generics/Main.java
+++ jdk8/generics/Main.java
@@ -6,5 +6,11 @@ public class Main {
         OrderedPair<String, String>  p2 = new OrderedPair<>("hello", "world");
         System.out.println(p1);
         System.out.println(p2);
+
+        Box<String> stringBox = new Box<>();
+        Box rawBox = stringBox; //ok
+
+        Box rawBox1 = new Box(); // rawBox is a raw type of Box<T>
+        Box<Integer> stringBox1 = rawBox; // warning: unchecked conversion
     }
 }
