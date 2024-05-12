d807612d0cec19df9c1cea8010062d1d1e8ae0f3
Bui Minh Toan <toan.bui@nexpando.com>
2024-05-12 19:50:14 +0700

    generics inheritance subtypes

---
+++ jdk8/generics/GenericsInheritanceSubtypes/Main.java
@@ -0,0 +1,9 @@
+package generics.GenericsInheritanceSubtypes;
+
+public class Main {
+    public static void main(String[] args) {
+        Object someObject = new Object();
+        Integer someInteger = new Integer(10);
+        someObject = someInteger;
+    }
+}
