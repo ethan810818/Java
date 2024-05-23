226a0a972d2ee3fcecb18eba9cd603c788298579
Bui Minh Toan <toan.bui@nexpando.com>
2024-05-23 22:16:39 +0700

    Add Upper Bounded Wildcards

--- jdk8/Wildcards/Wildcards.java
+++ jdk8/Wildcards/Wildcards.java
@@ -2,6 +2,14 @@ package Wildcards;
 
 import java.util.List;
 
+import NumberAndStrings.strings.ComputeInitials;
+
 public class Wildcards {
     public static void process(List<? extends Foo> list) {}
+
+    public static void process(List<? extends Foo> list, List<? extends Object> map) {
+        for (Foo elem : list) {
+            System.out.println(elem);
+        }
+    }
 }
