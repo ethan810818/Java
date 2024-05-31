95855af0a7a1866f363000cee719184c698e82a0
Bui Minh Toan <toan.bui@nexpando.com>
2024-05-31 15:24:48 +0700

    Add Erasure of Generic Types

---
+++ jdk8/TypeErasure/Node.java
@@ -0,0 +1,14 @@
+package TypeErasure;
+
+public class Node {
+    private Comparable data;
+    private Node next;
+
+    public Node(Comparable data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Comparable getData() { return data; }
+    
+}
